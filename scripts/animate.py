3bca9bc060cc3d8ba2e60071453e7895d69c89e9
Runbo Li <runboli2117@gmail.com>
2023-07-30 15:04:53 -0400

    Update animate.py

--- scripts/animate.py
+++ scripts/animate.py
@@ -53,7 +53,7 @@ def main(args):
     config = OmegaConf.load(args.config)
     samples = []
 
-    sample_idx = scene_number
+    sample_idx = args.scene_number
 
     print('Made it to line 54')
     for model_idx, (config_key, model_config) in enumerate(list(config.items())):
